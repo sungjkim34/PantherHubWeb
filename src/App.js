42ea0cdc949a6be8a31d8cd17c94a4535ff74d5d
sungjkim34 <sungjkim34@gmail.com>
2018-02-02 20:35:28 -0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './Containers/Home/Home';
+import { Switch, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">PantherHub Web</h1>
+          <Link to='/test'>Test</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/test' component={Home}/>
+        </Switch>
       </div>
     );
   }
