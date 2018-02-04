69b95811372fcfc390f5c9eab7f9e0646cf33c57
sungjkim34 <sungjkim34@gmail.com>
2018-02-03 22:42:31 -0500

    add grids and cards

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { getUserInfo } from '../../Services/UserService';
-import { Button, Form, Header, Image, Menu } from 'semantic-ui-react';
+import { Button, Card, Form, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react';
 import { Switch, Route, Link } from 'react-router-dom';
 import './Home.css';
 
@@ -27,7 +27,7 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div className="home-container">
+            <div className="home-page">
                 <Menu stackable borderless style={{ backgroundColor: '#fff', border: '1px solid #ddd', boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',}}>
                         <Menu.Item>
                             <Image src={require('../../Assets/GeorgiaStateFlatLogo.png')} size='medium' />
@@ -37,12 +37,34 @@ export default class Home extends Component {
                         <Menu.Item as={Link} to='/'>Finances</Menu.Item>
                         <Menu.Item as={Link} to='/'>Class</Menu.Item>
                         <Menu.Item position='right'>
-                            <span style={{marginRight: '10px'}}>Welcome {this.state.userInfo.firstName}</span>
+                            <span style={{marginRight: '10px'}}>{this.state.userInfo.firstName}</span>
                             <Button onClick={() => this.logout()} content='Logout' secondary/>
                         </Menu.Item>
                 </Menu>
-                <Header as='h2'>PantherHub</Header>
-                { this.state.userInfo && <div>Welcome {this.state.userInfo.firstName}</div> }
+                <div className="home-container">
+                    <Header as='h2'>PantherHub - Enrollment</Header>
+                    <Card fluid header='Student Dashboard' description={`Welcome ${this.state.userInfo.firstName} ${this.state.userInfo.lastName}`}/>
+                    <Grid columns={3} divided>
+                        <Grid.Row stretched>
+                        <Grid.Column>
+                            <Segment>
+                                <Header as='h4'>My Registration</Header>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Segment>
+                                <Header as='h4'>My Bill</Header>
+                                <p>Total Due:</p>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Segment>
+                                <Header as='h4'>My Financial Aid</Header>
+                            </Segment>
+                        </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
             </div>
         );
     }
